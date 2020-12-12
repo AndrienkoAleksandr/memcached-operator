84a47260b52c587211c2cd4ef44eb4e8a7aeb799
Oleksandr Andriienko <oandriie@redhat.com>
2020-12-12 21:16:43 +0200

    Fix test.
    Signed-off-by: Oleksandr Andriienko <oandriie@redhat.com>

--- controllers/memcached_controller_test.go
+++ controllers/memcached_controller_test.go
@@ -1,18 +1,28 @@
 package controllers
 
 import (
-    "context"
-    "testing"
+	"context"
+	"testing"
 
-    cachev1alpha1 "github.com/AndrienkoAleksandr/memcached-operator/pkg/apis/cache/v1alpha1"
-    "k8s.io/apimachinery/pkg/runtime"
-    "sigs.k8s.io/controller-runtime/pkg/client"
-    "sigs.k8s.io/controller-runtime/pkg/client/fake"
+	cachev1alpha1 "github.com/AndrienkoAleksandr/memcached-operator/api/v1alpha1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/kubernetes/scheme"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 )
 
 func TestMemcachedController(t *testing.T) {
     // A Memcached object with metadata and spec.
+
+    scheme := scheme.Scheme
+	scheme.AddKnownTypes(cachev1alpha1.GroupVersion, &cachev1alpha1.Memcached{}, &cachev1alpha1.MemcachedList{})
+
     memcached := &cachev1alpha1.Memcached{
+        TypeMeta: metav1.TypeMeta {
+            APIVersion: "cache.example.com/v1alpha1",
+            Kind: "Memcached",
+        },
         ObjectMeta: metav1.ObjectMeta{
             Name:      "memcached",
             Namespace: "memcached-operator",
